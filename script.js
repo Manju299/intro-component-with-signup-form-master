002041e188f74399babf8e673d275a1a43ad37f5
Manjunath.Hosamani <manjunathhosamani67@gmail.com>
2023-12-19 00:11:50 +0530

    final submission

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function validate(e){
     const lastName = document.getElementById("lname");
     const email = document.getElementById("email");
     const password = document.getElementById("pass");
+    const errorIcons = document.querySelectorAll(".er_ricon")
+    console.log(errorIcons.length);
     // const errorIcon = firstName.nextElementSibling;
     var mailformat = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/";
     // let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -22,10 +24,14 @@ function validate(e){
     if(firstName.value === "" ){
         const err = document.getElementById("nameErrId");
         err.removeAttribute('hidden');
+        errorIcons[0].style.display = 'block';
+        
         // firstName.classList.add("fname");
         // errorIcon.style.display = "block";
         // err.textContent = "First name cannot be empty"
         firstName.style.borderColor="red";
+    
+
           
     }
     
@@ -33,12 +39,15 @@ function validate(e){
         const err1 = document.getElementById("lnameErrId");
         err1.removeAttribute('hidden');
         lastName.style.borderColor="red";
+        errorIcons[1].style.display = 'block';
+
     
     }
     if(email.checkValidity()){
         const err2 = document.getElementById("emailErrId");
         err2.removeAttribute('hidden');
         email.style.borderColor="red";  
+        errorIcons[2].style.display = 'block';
         // email.style.value.color="red";   
     
     }
@@ -46,13 +55,13 @@ function validate(e){
         const err2 = document.getElementById("emailErrId");
         err2.removeAttribute('hidden');
         email.style.borderColor="red";
-
-        
+        errorIcons[2].style.display = 'block';
 
     }
     if(password.value === "" ){
         const err3 = document.getElementById("passErrId");
         err3.removeAttribute('hidden');
+        errorIcons[3].style.display = 'block';
         password.style.borderColor="red";
     
     }
